1b4aa60c05171d68b2a6a216448bac7da1519a0b
Imanol <imanoleas@gmail.com>
2015-01-17 20:30:56 +0100

    Añadida funcionalidad a la barra de búsqueda

--- gameboy_world/games/urls.py
+++ gameboy_world/games/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.GameIndex.as_view(), name="index"),
+    url(r'^search/$', views.search, name="search"),
     url(r'^(?P<slug>\S+)$', views.GameDetail.as_view(), name="game_detail"),
 )
--- gameboy_world/games/views.py
+++ gameboy_world/games/views.py
@@ -1,11 +1,22 @@
 from django.views import generic
+from django.shortcuts import render
 from . import models
 
 class GameIndex(generic.ListView):
 	queryset = models.Game.objects.order_by('title')
 	template_name = "home.html"
-	paginate_by = 100
+	paginate_by = 20
 
 class GameDetail(generic.DetailView):
 	model = models.Game
 	template_name = "post.html"
+
+def search(request):
+    query = request.GET.get('q')
+    if query:
+        # There was a query entered.
+        results = models.Game.objects.filter(title__contains=query)
+    else:
+        # If no query was entered, simply return all objects
+        results = models.Game.objects.order_by('title')
+    return render(request, "search.html", {"results": results})
