9574bb2339a17da79a86806cfd7c8ecd8a2f741e
Imanol <imanoleas@gmail.com>
2015-01-18 16:26:10 +0100

    Static files6

--- gameboy_world/settings.py
+++ gameboy_world/settings.py
@@ -84,5 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
+STATIC_URL = '/static/'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
